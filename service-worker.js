ca5e3f4b104cc1497dd0381c94d86cd68c454105
Emilio Neva <ejnero-dev@users.noreply.github.com>
2025-11-06 14:24:37 +0100

    Add: Footer profesional con información de contacto y privacidad

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'activacion-conductual-v5';
+const CACHE_NAME = 'activacion-conductual-v6';
 const urlsToCache = [
   '/behavioral-activation/',
   '/behavioral-activation/index.html',
